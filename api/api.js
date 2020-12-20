a6287077afb92f64af56fb1aee1292bb5ed9f5dc
José Antonio Díaz Ramírez <73711750+Joseadr99@users.noreply.github.com>
2020-12-20 19:31:15 +0100

    Update api.js

--- api/api.js
+++ api/api.js
@@ -283,15 +283,15 @@ module.exports = (req, res) => {
 					let horasDisponibles = []
 					let aux = 0
 
-					/*for(let i=0; i < numHorasDisponibles; i++){
+					for(let i=0; i < numHorasDisponibles; i++){
 						aux = generarEntero(0,horas.length-1)
 						if (horasDisponibles.findIndex(horas[aux]) == -1){
 							horasDisponibles.push(horas[aux])
-							//devolucion = devolucion +"las "+ JSON.stringify(horas[aux]) + ", "
+							//devolucion = devolucion +"las "+ horas[aux] + ", "
 						}else{
 							i = i-1
 						}
-					}*/
+					}
 					
 						 
 
